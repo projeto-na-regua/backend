d8229a059001010876838e0f53df224575c72f6a
lucasfari-a <lucas.flima@sptech.school>
2024-03-20 17:51:27 -0300

    @lucasfari-a SpringBoot e Java

---
+++ SpringBoot - Java/src/main/java/projetopi/projetopi/Barbeiro.java
@@ -0,0 +1,9 @@
+package projetopi.projetopi;
+
+public class Barbeiro extends Usuario{
+    public Barbeiro(String nome, String telefone, String email) {
+        super(nome, telefone, email);
+    }
+
+
+}
