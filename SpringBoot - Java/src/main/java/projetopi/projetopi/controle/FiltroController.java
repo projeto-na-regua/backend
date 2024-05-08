9b9ef634910a2b39e46ab34e34ce6240b884be33
Melissa Neves <melissa.neves@sptech.school>
2024-05-08 10:47:16 -0300

    Atualização ordenação

--- SpringBoot - Java/src/main/java/projetopi/projetopi/controle/FiltroController.java
+++ SpringBoot - Java/src/main/java/projetopi/projetopi/controle/FiltroController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import projetopi.projetopi.dominio.api.Precipitacao;
@@ -13,6 +14,11 @@ import projetopi.projetopi.dominio.api.Temperatura;
 import projetopi.projetopi.dto.mappers.PrevisaoMapper;
 import projetopi.projetopi.dto.response.PrevisaoApi;
 import projetopi.projetopi.service.FiltrosService;
+import projetopi.projetopi.util.ListaObj;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.http.ResponseEntity.*;
 
 @RestController
 @RequestMapping("/filtros")
@@ -29,100 +35,77 @@ public class FiltroController {
             """)
     @ApiResponse(responseCode = "200", description = "Previsão")
 
-    public ResponseEntity<PrevisaoApi[]> getTemperatura() {
+    public ResponseEntity<ListaObj<PrevisaoApi>> getTemperatura() {
 
         Temperatura t = service.getTemperatura();
         Precipitacao p = service.getPrecipitacao();
 
         if (service.getTemperatura() == null) {
-            return ResponseEntity.noContent().build();
+            return noContent().build();
         }
 
         if (service.getPrecipitacao() == null) {
-            return ResponseEntity.noContent().build();
+            return noContent().build();
         }
-        return ResponseEntity.ok(new PrevisaoMapper().toDto(t, p));
+        return ok(new PrevisaoMapper().toDto(t, p));
 
     }
 
 
-    @GetMapping("/horario-temperaturas-altas")
+    @GetMapping("/horario/{tipoFiltro}")
     @Operation(summary = "Previsão de tempo próximos 7 dias", description = """
             # Listar o horario e data, precipitação e temperatura utilizando uma API externa
             ---
             Retorna um vetor com a precipitação e temperatura das próximas 168 horas.
             """)
     @ApiResponse(responseCode = "200", description = "Previsão")
-    public ResponseEntity<PrevisaoApi[]> getTemperaturaAltas() {
+    public ResponseEntity<ListaObj<PrevisaoApi>> getTemperatura(@PathVariable String tipoFiltro) {
 
         Temperatura t = service.getTemperatura();
         Precipitacao p = service.getPrecipitacao();
 
         if (service.getTemperatura() == null) {
-            return ResponseEntity.noContent().build();
+            return noContent().build();
         }
 
         if (service.getPrecipitacao() == null) {
-            return ResponseEntity.noContent().build();
+            return noContent().build();
         }
 
-        PrevisaoApi[] v = new PrevisaoMapper().toDto(t, p);
 
-        service.ordenarPorTemperaturasAltas(v, 0, v.length -1);
-        return ResponseEntity.ok(v);
+        ListaObj<PrevisaoApi> v = new PrevisaoMapper().toDto(t, p);
+
+        if(tipoFiltro.equals("temperatura-alta")){
+            service.ordenarPrevisao(v, 0, v.getTamanho() -1, true, "t");
+        }else if (tipoFiltro.equals("temperatura-baixa")){
+            service.ordenarPrevisao(v, 0, v.getTamanho() -1, false, "t");
+        }else if(tipoFiltro.equals("precipitacao")){
+            service.ordenarPrevisao(v,0, v.getTamanho() -1, false, "p");
+        }
+
+        return ok(v);
 
     }
 
-    @GetMapping("/horario-temperaturas-baixas")
-    @Operation(summary = "Previsão de tempo próximos 7 dias", description = """
-            # Listar o horario e data, precipitação e temperatura utilizando uma API externa
-            ---
-            Retorna um vetor com a precipitação e temperatura das próximas 168 horas.
-            """)
-    @ApiResponse(responseCode = "200", description = "Previsão")
-    public ResponseEntity<PrevisaoApi[]> getTemperaturaBaixas() {
+    @GetMapping("/pesquisa/{dataHora}")
+    public ResponseEntity<PrevisaoApi> getHora(@PathVariable LocalDateTime dataHora){
+
 
         Temperatura t = service.getTemperatura();
         Precipitacao p = service.getPrecipitacao();
 
         if (service.getTemperatura() == null) {
-            return ResponseEntity.noContent().build();
+            return noContent().build();
         }
 
         if (service.getPrecipitacao() == null) {
-            return ResponseEntity.noContent().build();
+            return noContent().build();
         }
 
-        PrevisaoApi[] v = new PrevisaoMapper().toDto(t, p);
+        ListaObj<PrevisaoApi> lista = PrevisaoMapper.toDto(t, p);
+        int resultado = service.pesquisaBinaria(lista, dataHora);
 
-        service.ordenarPorTemperaturasBaixas(v, 0, v.length -1);
-        return ResponseEntity.ok(v);
-    }
-
-    @GetMapping("/horario-precipitacao-baixa")
-    @Operation(summary = "Previsão da precipitação dos próximos 7 dias", description = """
-            # Listar o horario e data, precipitação e temperatura utilizando uma API externa
-            ---
-            Retorna um vetor com a precipitação e temperatura das próximas 168 horas.
-            """)
-    @ApiResponse(responseCode = "200", description = "Previsão")
-    public ResponseEntity<PrevisaoApi[]> getPrecipitacaoBaixa() {
-
-        Temperatura t = service.getTemperatura();
-        Precipitacao p = service.getPrecipitacao();
-
-        if (service.getTemperatura() == null) {
-            return ResponseEntity.noContent().build();
-        }
-
-        if (service.getPrecipitacao() == null) {
-            return ResponseEntity.noContent().build();
-        }
-
-        PrevisaoApi[] v = new PrevisaoMapper().toDto(t, p);
-
-        service.ordenarPorPrecipitacao(v, 0, v.length -1);
-        return ResponseEntity.ok(v);
+        return resultado != -1 ? status(200).body(lista.getElemento(resultado)) : status(404).build();
     }
 
 
