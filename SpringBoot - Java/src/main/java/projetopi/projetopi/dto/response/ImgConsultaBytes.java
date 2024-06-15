2e226c556aaa7cb91d1cd16b79195f123d44c913
lucasfari-a <lucas.flima@sptech.school>
2024-06-14 21:36:42 -0300

    @lucasfari-a

---
+++ SpringBoot - Java/src/main/java/projetopi/projetopi/dto/response/ImgConsultaBytes.java
@@ -0,0 +1,10 @@
+package projetopi.projetopi.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ImgConsultaBytes {
+    private byte[] imgConsultaArray;
+}
