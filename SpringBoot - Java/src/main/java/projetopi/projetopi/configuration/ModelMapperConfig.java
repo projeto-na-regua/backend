6e8ac2077cf984b372aa75a22c4f1058e43c286c
melissaSNeves <145376881+MelissaSNeves@users.noreply.github.com>
2024-11-24 21:43:16 -0300

    Add config modelmapper

--- SpringBoot - Java/src/main/java/projetopi/projetopi/configuration/ModelMapperConfig.java
+++ SpringBoot - Java/src/main/java/projetopi/projetopi/configuration/ModelMapperConfig.java
@@ -1,4 +1,21 @@
 package projetopi.projetopi.configuration;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        // Você pode configurar o ModelMapper aqui, se necessário.
+        ModelMapper modelMapper = new ModelMapper();
+
+        // Configuração opcional para ajuste de mapeamentos (caso precise):
+        // modelMapper.getConfiguration().setFieldMatchingEnabled(true)
+        //         .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+
+        return modelMapper;
+    }
 }
