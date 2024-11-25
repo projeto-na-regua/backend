8690b43f92d0dd61156fe9615d09bd2b3c731a14
melissaSNeves <145376881+MelissaSNeves@users.noreply.github.com>
2024-11-24 21:37:01 -0300

    Update

--- SpringBoot - Java/src/main/java/projetopi/projetopi/configuration/SwaggerConfig.java
+++
@@ -1,8 +0,0 @@
-package projetopi.projetopi.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SwaggerConfig {
-    // Esta classe pode estar vazia, pois o SpringDoc detecta automaticamente os controllers.
-}
